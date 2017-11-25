d67b029a73639c933c3c871870eb97f3f95c4324
mrvu.vnn <mrvu.vnn@gmail.com>
2017-11-25 12:59:29 +0700

    Làm quen với State và sự kiện onclick

---
+++ vidu_reactjs/public/js/vidu1.js
@@ -0,0 +1,20 @@
+var Com = React.createClass({
+  add(){
+    this.setState({num: this.state.num + 1});
+  },
+
+  getInitialState(){
+      return{num: 0};
+  },
+
+  render(){
+    return(
+      <button onClick={this.add}>Hello {this.state.num}</button>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Com/>
+  ,document.getElementById('root')
+);
